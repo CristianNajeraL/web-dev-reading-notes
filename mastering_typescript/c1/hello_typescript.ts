876ea27079f6233909ea3a9a8e631a475072023b
cristianenajera@gmail.com <*O+0clqH5dR@]mC]{1w!|6:P]&/.XFo}Mp0HtAlAQ&~Q:@{a(^iVn'H;jggA)!y!Gcff5\lc>
2022-05-04 10:04:30 -0700

    feat: chapter 1 typescript intro

---
+++ mastering_typescript/c1/hello_typescript.ts
@@ -0,0 +1,14 @@
+import * as inquirer from "inquirer";
+
+inquirer.prompt(
+    [
+        {
+            name: "first_name",
+            message: "What is your name?"
+        }
+    ]
+).then(
+    answers => {
+        console.log(`You answered ${answers.first_name}`);
+    }
+);
